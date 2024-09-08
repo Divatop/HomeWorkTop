1c636e2f65f844966f92c5e23ad45ed52afac528
Divatop <prusov1995@inbox.ru>
2024-09-08 11:12:06 +0500

    HomeWork

---
+++ src/HomeWork.java
@@ -0,0 +1,21 @@
+public class HomeWork {
+
+    public static void main(String[] args) {
+        byte byte1 = 125;
+        short short1 = 23_457;
+        int int1 = 2_345_457;
+        long long1 = 3_678_265_984L;
+        float float1 = 3.45F;
+        double double1 = 15.65;
+        char char1 = 's';
+        boolean is1 = true;
+
+
+
+
+
+
+
+
+   }
+}
