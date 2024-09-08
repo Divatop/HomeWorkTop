e11652ce2c744168d9fffb637580f24e48d9c478
Divatop <prusov1995@inbox.ru>
2024-09-08 11:32:31 +0500

    HomeWork

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,14 +1,32 @@
 public class HomeWork {
 
-    public static void main(String[] args) {
-        byte byte1 = 125;
-        short short1 = 23_457;
-        int int1 = 2_345_457;
-        long long1 = 3_678_265_984L;
-        float float1 = 3.45F;
-        double double1 = 15.65;
-        char char1 = 's';
-        boolean is1 = true;
+   public static void main(String[] args) {
+ //       byte byte1 = 125;
+ //       short short1 = 23_457;
+ //       int int1 = 2_345_457;
+ //       long long1 = 3_678_265_984L;
+ //       float float1 = 3.45F;
+ //       double double1 = 15.65;
+ //       char char1 = 's';
+//        boolean is1 = true;
+
+
+
+                int a = 20;
+                int b = 5;
+
+                int primer1 = a + b;
+                int primer2 = a - b;
+                int primer3 = a * b;
+                int primer4 = a / b;
+                int primer5 = a % b;
+
+                System.out.println("Сложение: " + primer1);
+                System.out.println("Вычитание: " + primer2);
+                System.out.println("Умножение: " + primer3);
+                System.out.println("Деление: " + primer4);
+                System.out.println("Остаток от деления: "  + primer5);
+
 
 
 
